ba371d591f236ec9ab3797ebe72bdf8b009a3c31
Garayev Vasif <70977548+vasifcoding@users.noreply.github.com>
2023-01-02 18:13:17 +0300

    Add files via upload

---
+++ loginsistemi/loginsistemi.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Anamakine: 127.0.0.1
+-- Üretim Zamanı: 02 Oca 2023, 16:12:32
+-- Sunucu sürümü: 10.4.25-MariaDB
+-- PHP Sürümü: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Veritabanı: `loginsistemi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `isim` varchar(300) COLLATE utf8_turkish_ci NOT NULL,
+  `soyisim` varchar(300) COLLATE utf8_turkish_ci NOT NULL,
+  `email` varchar(300) COLLATE utf8_turkish_ci NOT NULL,
+  `parola` varchar(300) COLLATE utf8_turkish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Tablo döküm verisi `login`
+--
+
+INSERT INTO `login` (`id`, `isim`, `soyisim`, `email`, `parola`) VALUES
+(13, 'kullanici1', 'soyisim', 'email@gmail.com', 'parola123');
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
